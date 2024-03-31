64f1bc34d2f166fe1fccb88889d4f0645ebd384e
BlackTea-c <2286554510@qq.com>
2024-03-31 20:51:35 +0800

    3/31

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 
+import torch
+import torch.nn as nn
+
+# 假设你有一个类别特征，取值范围为 0 到 9
+# 我们定义了一个嵌入层，输入大小为 10（类别的总数），输出大小为 3（嵌入向量的维度）
+embedding = nn.Embedding(6, 3)
+
+# 创建一个示例输入，其中包含了 5 个样本，每个样本的特征值都在 0 到 9 之间
+input_data = torch.LongTensor([[1, 2, 3, 4, 5]])
+
+# 将输入传递给嵌入层
+embedded_data = embedding(input_data)
+
+# 输出嵌入后的数据
+print("Embedded Data:")
+print(embedded_data)
--- model&code/双塔模型.py
+++ model&code/双塔模型.py
@@ -1,6 +1,26 @@
-import torch
+
 import pandas as pd
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
 
 
-data=pd.read_csv('testdata/ml-latest-small/movies.csv')
-print(data.columns)
+moive_data=pd.read_csv('testdata/ml-latest-small/movies.csv')
+user_data=pd.read_csv('testdata/ml-latest-small/ratings.csv')
+
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(3, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        logits = self.linear_relu_stack(x)
+        return logits
